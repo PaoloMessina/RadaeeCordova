645446a451f4159abd1be6cfe68af8e7fd87c276
Paolo Messina <phemt.latd@gmail.com>
2015-07-31 12:19:04 +0200

    Full import Library in Android
    Full import Library in Android

---
+++ android/RPDFViewActivity.java
@@ -0,0 +1,31 @@
+package it.almaviva.cordovaplugins;
+
+import android.app.Activity;
+import android.content.res.AssetManager;
+import android.opengl.GLSurfaceView;
+import android.opengl.GLSurfaceView.Renderer;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.common.io.ByteStreams;
+import com.radaee.pdf.DIB;
+import com.radaee.pdf.Document;
+import com.radaee.pdf.Global;
+import com.radaee.pdf.Matrix;
+import com.radaee.pdf.Page;
+import com.radaee.util.PDFAssetStream;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+/**
+ * Created by pmessina on 30/07/15.
+ */
+public class RPDFViewActivity {
+}
